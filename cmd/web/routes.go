e6fabd88c707bb95e30135cfc4db951e2fc015b4
RubaCodes <rubacodes@gmail.com>
2024-08-10 11:56:55 +0200

    end chapter 6 - middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,18 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes.
-func (app *application) routes(staticDir string) *http.ServeMux {
+func (app *application) routes(staticDir string) http.Handler {
 	mux := http.NewServeMux()
 	/** File Server Configuration*/
 	fileServer := http.FileServer(http.Dir(staticDir))
 	//For matching paths, we strip the "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	// handlers
-
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(mux)
 }
